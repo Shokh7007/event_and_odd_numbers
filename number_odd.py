8efc00bd026c41a4ea31eb3f5919df38d15c247f
Shokh7007 <shoxruxbeknafasov9@gmail.com>
2023-04-14 14:06:41 +0500

    n odd

--- number_odd.py
+++ number_odd.py
@@ -2,3 +2,9 @@
 #Create a variable "var_int" and assign it a four-digit integer value.
 
 #Print the number of odd digits in the variable "var_int".
+x=1234
+a=x%10
+b=x%100//10
+c=x%1000//100
+d=x%10000//1000
+print(b+d)
